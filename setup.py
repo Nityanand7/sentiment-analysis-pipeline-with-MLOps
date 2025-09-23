60195eea8575104574166ed07f416d9b1e99fdb9
Nityanand7 <msnityanand997@gmail.com>
2025-09-22 19:57:07 -0700

    updating code changes upto data_preprocessing

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'mlops-pipeline',
+    version= '0.0.0',
+    author= 'Nityanand',
+    author_email= 'nityanandmadpathi@gmail.com',
+    packages= find_packages(),
+    install_requires = [],
+    python_requires=">=3.12, <3.13"
+)
